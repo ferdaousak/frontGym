6a9c577961eebdb236cbb2a4e8ef2c9b09e2c9cd
ferdaousak <akkar88889@gmail.com>
2021-02-18 12:54:18 +0100

    fixed some problems

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -20,7 +20,7 @@ class UserService {
     }
 
     getModeratorBoard() {
-        return axios.get(API_URL + 'mod', { headers: authHeader() });
+        return axios.get(API_URL + 'trainer', { headers: authHeader() });
     }
 
     getAdminBoard() {
